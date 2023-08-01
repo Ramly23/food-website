5380c85636df53252d8afd751a0909cedbf63de7
Justin Cho Chun Wah <chowah67@gmail.com>
2023-08-01 21:21:29 +0800

    Done Dynamic Paging

--- js/main.js
+++ js/main.js
@@ -195,7 +195,6 @@ const sr = ScrollReveal({
   distance: "60px",
   duration: 2500,
   delay: 400,
-  reset: false,
 });
 
 sr.reveal(`.swiper, .section__title, .section__subtitle, .cards__content`);
